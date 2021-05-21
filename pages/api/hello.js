3ea3a6e4d25f14b3bbd0d63c83841e8b54b4f00a
Nitto <nitto_kun@users.noreply.github.com>
2021-05-21 10:54:14 -0300

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+  res
+    .status(200)
+    .json({
+      name: "John Doe",
+      idade: "18",
+      sexo: "masculino",
+      sobrenome: "Santos"
+    });
+};
